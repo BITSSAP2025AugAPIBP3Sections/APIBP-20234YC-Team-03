998761a4b1b0eacecccb8940b14f367d3b983aa1
Kousik Das <kousik.das@sap.com>
2025-10-29 12:14:33 +0530

    Initial Repo Setup

---
+++ DwellIn/src/main/java/org/apibp/dwellin/DwellInApplication.java
@@ -0,0 +1,13 @@
+package org.apibp.dwellin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwellInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwellInApplication.class, args);
+    }
+
+}
---
+++ DwellIn/src/test/java/org/apibp/dwellin/DwellInApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apibp.dwellin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwellInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
