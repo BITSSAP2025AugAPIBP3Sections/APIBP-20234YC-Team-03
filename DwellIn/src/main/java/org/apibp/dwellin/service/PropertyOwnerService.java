6cfb2e5aab8bc57c0fd908b227b6a8c223a1b7b7
Pallavi S <pallavis.2110@gmail.com>
2025-11-28 21:53:51 +0530

    adding service files

---
+++ DwellIn/src/main/java/org/apibp/dwellin/service/PropertyOwnerService.java
@@ -0,0 +1,11 @@
+package org.apibp.dwellin.service;
+
+public class PropertyOwnerService {
+
+    //register
+    //validate owner - using aadhar api
+    //get validation status
+    //login
+    //
+    
+}
---
+++ DwellIn/src/main/java/org/apibp/dwellin/service/UserService.java
@@ -0,0 +1,10 @@
+package org.apibp.dwellin.service;
+
+public class UserService {
+
+    //register
+    //login
+    //user validation either passport or aadhar - kousik
+    
+    
+}
